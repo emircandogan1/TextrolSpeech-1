235da11394f8da8bd2f6ea3e7b4079708a034419
emircan <eemircan.dogan@gmail.com>
2025-04-06 20:39:51 +0300

    changes

--- dataprocess.py
+++ dataprocess.py
@@ -7,25 +7,16 @@ import numpy as np
 from textgrid import TextGrid
 import pandas as pd
 
-### This script is recovered based on the chat history. If the threshold in the high and low does not match the result, Please recalculate the related label again and give feedback in the Github issue.
 
-
-
-###  pitch [136.57698522,196.09780757]   < low, normal, > high
 def gets_mean_pitch(wav_path):
 
-    # x, fs = sf.read(wav_path)
     x, fs = librosa.load(wav_path)
-    # print("*****",fs)
     x = x.astype(np.double)
-    # _f0_h, t_h = pw.harvest(x, fs)   #0.005s
     _f0_h, t_h = pw.dio(x, fs)
 
-    #得到基频
     f0_h = pw.stonemask(x, _f0_h, t_h, fs)
     data = f0_h
     mask = (data != 0)
-    # 计算每段非零的平均值 
     segments = np.split(data, np.where(np.diff(mask))[0]+1)
     non_zero_num,value = 0 ,0
     for seg in segments:
@@ -37,14 +28,12 @@ def gets_mean_pitch(wav_path):
       return 0,False
     return value/non_zero_num,True
 
-### energy [0.03331899,0.05054203]  < 0.03331899 low, normal, > 0.05054203 high
 def get_energy(wav_path):
     y, sr = librosa.load(wav_path)
     energy = librosa.feature.rms(y=y)
     energy = energy.mean()
     return energy
 
-###tempo [0.252,0.38645446]  该值表示平均每个单词的发音时间，< 0.252 high, normal, > 0.38645446 low
 def extract_tempo(textgrid_fp):
     itvs = TextGrid.fromFile(textgrid_fp)[0]
     num =0
@@ -54,8 +43,6 @@ def extract_tempo(textgrid_fp):
             continue
         num += 1
         dur += itvs[i].maxTime - itvs[i].minTime
-    # if num==0:
-    #     continue
     tempo = dur /num
 
 
